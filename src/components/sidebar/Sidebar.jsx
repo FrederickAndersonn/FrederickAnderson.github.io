609e673dcc431cbc7ad0e9234bfa76c2df55351f
FrederickAndersonn <frederickanderson71@yahoo.com>
2023-11-07 20:19:41 +0100

    smaller screen update

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,16 +2,6 @@ import React from 'react'
 import "./sidebar.css"
 import Logo from '../../assets/logo.png'
 
-// --first-color: hsl(353, 100%, 65%);
-// --title-color: hsl(244, 24%, 26%);
-// --text-color: hsl(244, 16%, 43%);
-// --body-color: hsl(258, 60%, 98%);
-// --container-color: #fff;
-// --dark-first-color: hsl(353, 100%, 65%);
-// --dark-title-color: hsl(244, 24%, 26%);
-// --dark-text-color: hsl(244, 16%, 43%);
-// --dark-body-color: hsl(258, 60%, 98%);
-// --dark-container-color: #fff;
 
 const Sidebar = () => {
     const setDarkMode = ()=>{
